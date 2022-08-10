f1a96f629c572ee0ade5d0005b128eb531821032
Rawle Juglal <rawledjuglal@gmail.com>
2022-08-09 22:07:07 -0500

    Card
    Working on hard coded card

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import react from 'react'
 import './App.css'
 import Navbar from '../Navbar/Navbar'
 import Hero from '../Hero/Hero'
+import Card from '../Card/Card'
 
 export default function App(){
   return(
@@ -9,6 +10,7 @@ export default function App(){
       <h1 className='--app-title'>AirBnB</h1>
       <Navbar />
       <Hero /> 
+      <Card />
     </div>   
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,12 +4,14 @@ import './Hero.css'
 export default function Hero(){
     return(
         <div className='--hero-container'>
-            <img className="--hero-img" src="https://res.cloudinary.com/relentlessrawle/image/upload/v1660074332/Group_77_sybcxo.png" alt="" />
-            <div className='--hero-text'>
-                <h2 className='--hero-title'>Online Experiences</h2>
+            <div className='--hero-img-container'>
+                <img className='--hero-img' src="https://res.cloudinary.com/relentlessrawle/image/upload/v1660074332/Group_77_sybcxo.png" alt="" />
+            </div>
+            <div className='--hero-text-container'>
+                <h1 className='--hero-title'>Online Experience</h1>
                 <p className='--hero-desc'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
-    </p>
-            </div>  
+</p>
+            </div>
         </div>
     )
 }
